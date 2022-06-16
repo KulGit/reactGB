d2ed1faafcebd9508934702e64bdec182c323033
KulGit <exciland@yandex.ru>
2022-06-16 19:51:03 +0300

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import Message from './Message';
+import { useEffect, useRef, useState } from 'react';
+// import Message from './Message';
 
 function App() {
-  const textMessage = "Create first React app";
+  const [messageList, setMessageList] = useState([])
+  const ref = useRef;
+  
+  console.log(messageList);
+
+
+  const sendMessage = () => {
+    let inputText = document.querySelector('#input').value;
+
+    console.log(inputText);
+
+    console.log(messageList);
+
+    //без ошибок, но не происходит обновление массива
+    setMessageList(ref.current.push('inputText'));
+
+    // при этом варианте добавление в массив только первый раз, а далее ошибка
+    // setMessageList(prev => prev.push('inputText'));
+
+  }
+
+  
+  useEffect(() => {
+    ref.current = [];
+  }, [])
+
   return (
     <div>
-      <Message name={textMessage}/>
+      <input id='input' ></input>
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
