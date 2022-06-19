d088f99f76ab6c6bc0daf354f141a13ed5d98ef1
KulGit <exciland@yandex.ru>
2022-06-19 22:03:26 +0300

    lesson2 is done

--- src/App.js
+++ src/App.js
@@ -2,37 +2,44 @@ import { useEffect, useRef, useState } from 'react';
 // import Message from './Message';
 
 function App() {
-  const [messageList, setMessageList] = useState([])
-  const ref = useRef;
+  const [messageList, setMessageList] = useState([ ]);
+  const refText = useRef(null);
+  const refAuthor = useRef(null);
   
-  console.log(messageList);
-
-
-  const sendMessage = () => {
-    let inputText = document.querySelector('#input').value;
-
-    console.log(inputText);
-
+  const handleSendMessage = () => {
+    if (!(refText.current && refAuthor.current) || !(refText.current.value && refAuthor.current.value)) {
+      return
+    }
+    
+    setMessageList(prev => [...prev, {text: refText.current.value, autor: refAuthor.current.value}]);
     console.log(messageList);
 
-    //без ошибок, но не происходит обновление массива
-    //
-    setMessageList(ref.current.push('inputText'));
-
-    // при этом варианте добавление в массив только первый раз, а далее ошибка
-    // setMessageList(prev => prev.push('inputText'));
-
   }
-
   
   useEffect(() => {
-    ref.current = [];
-  }, [])
+     if (messageList.length === 0) {
+      return
+    }
+    setTimeout(() => {
+      console.log(messageList.pop().autor)
+    }, 2000)
+
+  }, [messageList])
 
   return (
     <div>
-      <input id='input' ></input>
-      <button onClick={sendMessage}>Send Message</button>
+      <p>Your message</p>
+      <input ref={refText} />
+      <p>Who are you?</p>
+      <input ref={refAuthor} />
+      <button onClick={handleSendMessage}>Send Message</button>
+      <div>
+        {messageList.map((item, index) => {
+          return (
+            <div key={index}> TEXT:  {item.text} AUTHOR:  {item.autor}</div>
+          )
+        })}
+      </div>
     </div>
   );
 }
